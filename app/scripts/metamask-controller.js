902190ff56fdc631beaef6b7c838b278bda06354
Filip Sekulic <filip.sekulic@consensys.net>
2022-12-01 14:45:27 +0100

    What's new popover for tx security provider

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1805,6 +1805,10 @@ export default class MetamaskController extends EventEmitter {
         ),
       setFirstTimeUsedNetwork:
         appStateController.setFirstTimeUsedNetwork.bind(appStateController),
+      setOpenSeaTransactionSecurityProviderPopoverHasBeenShown:
+        appStateController.setOpenSeaTransactionSecurityProviderPopoverHasBeenShown.bind(
+          appStateController,
+        ),
       // EnsController
       tryReverseResolveAddress:
         ensController.reverseResolveAddress.bind(ensController),
