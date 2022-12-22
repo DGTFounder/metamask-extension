fdae23c6dbdfc3d6e7374580efad27fed2e85eb4
Filip Sekulic <filip.sekulic@consensys.net>
2022-12-22 15:13:29 +0100

    Fixed e2e test

--- test/e2e/fixture-builder.js
+++ test/e2e/fixture-builder.js
@@ -306,6 +306,7 @@ function onboardingFixture() {
           [CHAIN_IDS.GOERLI]: true,
           [CHAIN_IDS.LOCALHOST]: true,
         },
+        openSeaTransactionSecurityProviderPopoverHasBeenShown: true,
       },
       NetworkController: {
         network: '1337',
